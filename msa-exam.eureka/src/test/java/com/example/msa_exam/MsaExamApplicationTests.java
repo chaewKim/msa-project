ffaba1794d2fbc47c90398927c7e4cc28971375c
chaewKim <anjh7127@gmail.com>
2025-03-06 16:42:24 +0900

    init: MSA 프로젝트 세팅

---
+++ msa-exam.eureka/src/test/java/com/example/msa_exam/MsaExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
